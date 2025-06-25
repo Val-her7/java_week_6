85224a57717bf77c56f3e219d03e452fddea62e0
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-25 16:42:34 +0200

    feat: add role repository

---
+++ COGIP-API/src/main/java/dev/val/COGIP_API/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package dev.val.COGIP_API.repository;
+
+import dev.val.COGIP_API.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
