d3de893d7d7555e942f3212967b6049706d145f8
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-25 11:47:05 +0200

    add foundByUsername method to UserRepository

--- COGIP-API/src/main/java/dev/val/COGIP_API/repository/UserRepository.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/repository/UserRepository.java
@@ -4,6 +4,9 @@ import dev.val.COGIP_API.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
