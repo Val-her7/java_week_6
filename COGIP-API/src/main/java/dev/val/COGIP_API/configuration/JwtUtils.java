7cb15c3342b698a548cce9d7db027ff06a9bf42b
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-30 17:10:10 +0200

    feat: add JWT Token generator

---
+++ COGIP-API/src/main/java/dev/val/COGIP_API/configuration/JwtUtils.java
@@ -0,0 +1,45 @@
+package dev.val.COGIP_API.configuration;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+
+    @Value("${app.secret-key}")
+    private String secretKey;
+
+    @Value("${app.expiration-time}")
+    private long expirationTime;
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .and()
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        return new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+}
