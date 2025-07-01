90f1473e95dbb15f5e7754d14a1deb720d564651
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-01 16:08:15 +0200

    feat: add InvoiceDTOMapper

---
+++ COGIP-API/src/main/java/dev/val/COGIP_API/mapper/InvoiceDTOMapper.java
@@ -0,0 +1,32 @@
+package dev.val.COGIP_API.mapper;
+
+import dev.val.COGIP_API.dto.CompanySummaryDTO;
+import dev.val.COGIP_API.dto.ContactSummaryDTO;
+import dev.val.COGIP_API.dto.InvoiceDTO;
+import dev.val.COGIP_API.model.Invoice;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class InvoiceDTOMapper implements Function<Invoice, InvoiceDTO> {
+
+    @Override
+    public InvoiceDTO apply(Invoice invoice) {
+
+        CompanySummaryDTO companySummaryDTO = new CompanySummaryDTO(invoice.getCompany().getName(),
+                invoice.getCompany().getVatNumber(),
+                invoice.getCompany().getCompanyType());
+
+        ContactSummaryDTO contactSummaryDTO = new ContactSummaryDTO(invoice.getContact().getFirstName(),
+                invoice.getContact().getLastName(),
+                invoice.getContact().getEmail());
+
+        return new InvoiceDTO(
+                invoice.getNumber(),
+                invoice.getDate(),
+                companySummaryDTO,
+                contactSummaryDTO
+        );
+    }
+}
