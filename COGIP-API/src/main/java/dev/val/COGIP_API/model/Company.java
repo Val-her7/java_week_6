b215917b96356cd442823c5749fb00f8f54153ac
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-02 12:00:24 +0200

    fix: add initialization for List of contact and invoices in Company entity

--- COGIP-API/src/main/java/dev/val/COGIP_API/model/Company.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/model/Company.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -29,9 +31,9 @@ public class Company {
     private CompanyType companyType;
 
     @OneToMany(mappedBy = "company")
-    private List<Contact> contacts;
+    private List<Contact> contacts = new ArrayList<>();
 
     @OneToMany(mappedBy = "company")
-    private List<Invoice> invoices;
+    private List<Invoice> invoices = new ArrayList<>();
 
 }
