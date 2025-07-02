ef40693df3b55804ae431552c415e16b8723c4f3
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-02 10:25:30 +0200

    feat: add UserController with GET all users

---
+++ COGIP-API/src/main/java/dev/val/COGIP_API/controller/UserController.java
@@ -0,0 +1,26 @@
+package dev.val.COGIP_API.controller;
+
+import dev.val.COGIP_API.dto.UserResponseDTO;
+import dev.val.COGIP_API.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+}
