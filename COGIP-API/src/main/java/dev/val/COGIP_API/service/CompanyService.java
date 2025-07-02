c107fd19040038c8e843a35bdc69cd6b11a0774c
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-02 12:02:34 +0200

    feat: add createCompany method to CompanyService

--- COGIP-API/src/main/java/dev/val/COGIP_API/service/CompanyService.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/service/CompanyService.java
@@ -2,6 +2,8 @@ package dev.val.COGIP_API.service;
 
 import dev.val.COGIP_API.dto.CompanyDTO;
 import dev.val.COGIP_API.mapper.CompanyDTOMapper;
+import dev.val.COGIP_API.mapper.CompanyEntityMapper;
+import dev.val.COGIP_API.model.Company;
 import dev.val.COGIP_API.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,15 @@ public class CompanyService {
 
     private final CompanyRepository companyRepository;
     private final CompanyDTOMapper companyDTOMapper;
+    private final CompanyEntityMapper companyEntityMapper;
 
     public List<CompanyDTO> getAllCompanies() {
         return companyRepository.findAll().stream().map(companyDTOMapper).toList();
+    }
+
+    public CompanyDTO createCompany(CompanyDTO companyDTO) {
+        Company company = companyEntityMapper.apply(companyDTO);
+        Company saved = companyRepository.save(company);
+        return companyDTOMapper.apply(saved);
     }
 }
