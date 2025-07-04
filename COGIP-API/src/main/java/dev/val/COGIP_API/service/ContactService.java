db4bf6a3296ea184c1a5844f7001fbe016c0c3ce
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-04 12:24:28 +0200

    feat: add getContactById and createContact methods to ContactService

--- COGIP-API/src/main/java/dev/val/COGIP_API/service/ContactService.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/service/ContactService.java
@@ -2,6 +2,10 @@ package dev.val.COGIP_API.service;
 
 import dev.val.COGIP_API.dto.ContactDTO;
 import dev.val.COGIP_API.mapper.ContactDTOMapper;
+import dev.val.COGIP_API.mapper.ContactEntityMapper;
+import dev.val.COGIP_API.model.Company;
+import dev.val.COGIP_API.model.Contact;
+import dev.val.COGIP_API.repository.CompanyRepository;
 import dev.val.COGIP_API.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +18,27 @@ public class ContactService {
 
     private final ContactRepository contactRepository;
     private final ContactDTOMapper contactDTOMapper;
+    private final CompanyRepository companyRepository;
+    private final ContactEntityMapper contactEntityMapper;
 
     public List<ContactDTO> getAllContacts() {
         return contactRepository.findAll().stream().map(contactDTOMapper).toList();
+    }
+
+    public ContactDTO getContactById(int id) {
+        Contact contact = contactRepository.findById(id).orElse(null);
+
+        if(contact == null) return null;
+
+        return contactDTOMapper.apply(contact);
+    }
+
+    public ContactDTO createContact(ContactDTO contactDTO) {
+        Company company = companyRepository.findByName(contactDTO.companyName()).orElse(null);
+
+        if(company == null) return null;
+        Contact contact = contactEntityMapper.apply(contactDTO, company);
+        Contact saved = contactRepository.save(contact);
+        return contactDTOMapper.apply(saved);
     }
 }
