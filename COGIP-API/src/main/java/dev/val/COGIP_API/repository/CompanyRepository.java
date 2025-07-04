9a5ca8c81428fba5f705cd25858ae1cce0b863ea
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-04 12:23:42 +0200

    feat: add findByName method to company repo

--- COGIP-API/src/main/java/dev/val/COGIP_API/repository/CompanyRepository.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/repository/CompanyRepository.java
@@ -4,6 +4,9 @@ import dev.val.COGIP_API.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+    Optional<Company> findByName(String name);
 }
