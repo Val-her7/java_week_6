0cd0d4583a9d9ed4330bbc90e905c13693d08ed8
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-04 12:23:08 +0200

    fix: initialized List of invoices from contact

--- COGIP-API/src/main/java/dev/val/COGIP_API/model/Contact.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/model/Contact.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -33,5 +34,5 @@ public class Contact {
     private Company company;
 
     @OneToMany(mappedBy = "contact")
-    private List<Invoice> invoices;
+    private List<Invoice> invoices = new ArrayList<>();
 }
