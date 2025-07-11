3b4ba473dbf4b7e4f200d7d8a5a322c567cac7ec
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-11 14:06:18 +0200

    feat: add findByEmail method to contactRepository

--- COGIP-API/src/main/java/dev/val/COGIP_API/repository/ContactRepository.java
+++ COGIP-API/src/main/java/dev/val/COGIP_API/repository/ContactRepository.java
@@ -4,6 +4,9 @@ import dev.val.COGIP_API.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    Optional<Contact> findByEmail(String email);
 }
